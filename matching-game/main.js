ce3badbd3a1a5c72f3f84616eef6c1f981d607c6
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-04-27 13:18:38 -0300

    Add 5 more cards

--- matching-game/main.js
+++ matching-game/main.js
@@ -3,9 +3,17 @@ import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 
 import CardGame from "./src/components/CardGame";
+import PlayerName from "./src/components/PlayerName";
 
 const $root = document.querySelector("#root");
 const $htmlCardGame = CardGame();
+const $htmlPlayerName = PlayerName();
 
+$root.insertAdjacentHTML("afterbegin", $htmlPlayerName);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
 $root.insertAdjacentHTML("beforeend", $htmlCardGame);
 
