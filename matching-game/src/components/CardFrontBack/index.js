63b8463b429b33296cae81d9e538f543b6a67ec2
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-04-27 14:47:47 -0300

    Start  creation CardFrontBack component

---
+++ matching-game/src/components/CardFrontBack/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardFrontBack() {
+  return `
+    <article class="card-front-back">
+      <img src="./images/logo-javascript.png" alt="Logo JavaScript em pixelart">
+    </article>
+  `;
+}
+
+export default CardFrontBack;
