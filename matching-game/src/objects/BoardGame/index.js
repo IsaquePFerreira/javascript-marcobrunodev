70f8ce5865a57f81bbd2378260a07d6773319a79
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-05-01 09:46:27 -0300

    Create CardFrontBack component

--- matching-game/src/objects/BoardGame/index.js
+++ matching-game/src/objects/BoardGame/index.js
@@ -1,10 +1,13 @@
 import "./style.css";
 
 import CardGame from "../../components/CardGame";
+import CardFrontBack from "../../components/CardFrontBack";
 
 function BoardGame(amountCards) {
   const $htmlCardGame = CardGame();
-  const $htmlContent = $htmlCardGame.repeat(amountCards);
+  const $htmlCardFrontBack = CardFrontBack();
+  const $htmlContent = $htmlCardGame.repeat(amountCards) +
+    $htmlCardFrontBack.repeat(amountCards);
 
   return `
     <section class="board-game">
