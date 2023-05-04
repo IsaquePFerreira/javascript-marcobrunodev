adac8aadc3db215c14073d829439f7f61ced5193
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-05-04 09:53:28 -0300

    Create PlayerScore component

---
+++ matching-game/src/components/PlayerScore/index.js
@@ -0,0 +1,12 @@
+import "./style.css"
+function PlayerScore() {
+  return `
+    <div class="player-score">
+      <span class="square"></span>
+      <span class="square"></span>
+      <span class="square"></span>
+    </div>
+  `;
+}
+
+export default PlayerScore;
